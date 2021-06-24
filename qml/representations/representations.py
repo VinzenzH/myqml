d8a1ce0ff15ea9491024dd62054e3c0d3cdf94a2
VinzenzH <86363548+VinzenzH@users.noreply.github.com>
2021-06-24 10:37:45 +0200

    Update representations.py
    added generate_full_coulomb_matrix

--- qml/representations/representations.py
+++ qml/representations/representations.py
@@ -26,6 +26,7 @@ import numpy as np
 import itertools as itl
 
 from .frepresentations import fgenerate_coulomb_matrix
+from .frepresentations import fgenerate_full_coulomb_matrix
 from .frepresentations import fgenerate_unsorted_coulomb_matrix
 from .frepresentations import fgenerate_local_coulomb_matrix
 from .frepresentations import fgenerate_atomic_coulomb_matrix
@@ -112,6 +113,58 @@ def generate_coulomb_matrix(nuclear_charges, coordinates, size = 23, sorting = "
 
     if (sorting == "row-norm"):
         return fgenerate_coulomb_matrix(nuclear_charges, \
+            coordinates, size)
+
+    elif (sorting == "unsorted"):
+        return fgenerate_unsorted_coulomb_matrix(nuclear_charges, \
+            coordinates, size)
+
+    else:
+        print("ERROR: Unknown sorting scheme requested")
+        raise SystemExit
+        
+def generate_full_coulomb_matrix(nuclear_charges, coordinates, size = 23, sorting = "row-norm"):
+    """ Creates a Coulomb Matrix representation of a molecule.
+        Sorting of the elements can either be done by ``sorting="row-norm"`` or ``sorting="unsorted"``.
+        A matrix :math:`M` is constructed with elements
+
+        .. math::
+
+            M_{ij} =
+              \\begin{cases}
+                 \\tfrac{1}{2} Z_{i}^{2.4} & \\text{if } i = j \\\\
+                 \\frac{Z_{i}Z_{j}}{\\| {\\bf R}_{i} - {\\bf R}_{j}\\|}       & \\text{if } i \\neq j
+              \\end{cases},
+
+        where :math:`i` and :math:`j` are atom indices, :math:`Z` is nuclear charge and
+        :math:`\\bf R` is the coordinate in euclidean space.
+        If ``sorting = 'row-norm'``, the atom indices are reordered such that
+
+            :math:`\\sum_j M_{1j}^2 \\geq \\sum_j M_{2j}^2 \\geq ... \\geq \\sum_j M_{nj}^2`
+
+        The upper triangular of M, including the diagonal, is concatenated to a 1D
+        vector representation.
+
+        If ``sorting = 'unsorted``, the elements are sorted in the same order as the input coordinates
+        and nuclear charges.
+
+        The representation is calculated using an OpenMP parallel Fortran routine.
+
+        :param nuclear_charges: Nuclear charges of the atoms in the molecule
+        :type nuclear_charges: numpy array
+        :param coordinates: 3D Coordinates of the atoms in the molecule
+        :type coordinates: numpy array
+        :param size: The size of the largest molecule supported by the representation
+        :type size: integer
+        :param sorting: How the atom indices are sorted ('row-norm', 'unsorted')
+        :type sorting: string
+
+        :return: 1D representation - shape (size(size+1)/2,)
+        :rtype: numpy array
+    """
+
+    if (sorting == "row-norm"):
+        return fgenerate_full_coulomb_matrix(nuclear_charges, \
             coordinates, size)
 
     elif (sorting == "unsorted"):
